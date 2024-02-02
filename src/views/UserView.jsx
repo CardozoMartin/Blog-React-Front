c2e1d282eadeccd6fbba3a098ace6a4d4bb8107d
Martin Cardozo <tiin.-@hotmail.es>
2024-02-01 23:14:10 -0300

    se borra componente

--- src/views/UserView.jsx
+++ src/views/UserView.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { getPublicacionFn } from "../api/publicacion";
 import UserForm from "../components/users/UserForm";
 import UserCard from "../components/users/UserCard";
-import { Button } from "@/components/ui/button"
+
 const UserView = () => {
   const {
     data: publicacion,
